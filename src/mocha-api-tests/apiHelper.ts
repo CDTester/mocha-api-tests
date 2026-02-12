5f09e1653335f91af3897d583617b6894e2812df
CDTester <chris_a_day@hotmail.com>
2026-02-12 14:52:45 +0000

    code tidy up

--- src/mocha-api-tests/apiHelper.ts
+++ src/mocha-api-tests/apiHelper.ts
@@ -17,7 +17,6 @@ export interface apiConfig {
   accept: string;
   cacheControl: string,
   connection: string
-
 }
 
 export interface apiTest {
@@ -54,19 +53,21 @@ export interface apiTest {
  * A base class for API helper
  */
 export default class apiHelper {
-  protected instance: object;
-  protected baseUrl: string;
-  protected auth: string;
-  protected authType: string;
-  protected headers: object
-  protected cookies: string[];
-  protected redirect: number;
-  protected timeout: number;
-  protected proxy: any;
-  protected responseType: string
-  protected accept: string;
-  protected cacheControl: string;
-  protected connection: string;
+  private baseUrl: string;
+  private auth: string;
+  private authType: string;
+  private headers: object
+  private cookies: string[];
+  private redirect: number;
+  private timeout: number;
+  private proxy: any;
+  private responseType: string
+  private accept: string;
+  private cacheControl: string;
+  private connection: string;
+  private request: superagentTypes.Request;
+  private response: superagentTypes.Response;
+  
 
   constructor (config:apiConfig) {
     this.baseUrl = config.baseUrl;
@@ -81,38 +82,63 @@ export default class apiHelper {
     this.accept = config.accept;
     this.cacheControl = config.cacheControl;
     this.connection = config.connection;
-
+    this.request = superagent.requestBase();
   }
 
-  protected async mapReqResp (input: superagentTypes.Request, inputOrig:object, output: superagentTypes.Response, requestTime: string, responseTime: string): Promise<apiTest> {
-
+  private async mapReqResp (inputOrig:object, requestTime: string, responseTime: string): Promise<apiTest> {
     const request:apiTest['request'] = {
       time: requestTime,
       method: inputOrig['method'],
       headers: inputOrig['header'],
       cookies: inputOrig['cookies'],
-      port: input.req['agent']['defaultPort'],
+      port: this.request.req['agent']['defaultPort'],
       url: inputOrig['url'],
       queries: inputOrig['_query'],
       body: inputOrig['_data'],
       formData: inputOrig['_formData'],
       maxRedirects: inputOrig['_maxRedirects'],
-      redirects: { numberOfRedirects: input['_redirects'], redirectList: input['_redirectList'] },
-      files: input['files']
+      redirects: { 
+        numberOfRedirects: this.request['_redirects'], 
+        redirectList: this.request['_redirectList'] 
+      },
+      files: this.request['files']
     };
 
     const response:apiTest['response'] = {
       time: responseTime,
-      headers: output.headers,
-      buffered: output['buffered'],
-      redirects: output.redirects,
-      status: output.status,
-      statusText: output['res']['statusMessage'],
-      data: output.body,
-      files: output.files
+      headers: this.response.headers,
+      buffered: this.response['buffered'],
+      redirects: this.response.redirects,
+      status: this.response.status,
+      statusText: this.response['res']['statusMessage'],
+      data: this.response.body,
+      files: this.response.files
     };
 
     return { request, response };
+  }
+
+  private setAuth() {
+    if (this.authType !== undefined) {
+      if (this.authType === 'basic') this.request.set('Authorization', `Basic ${this.auth}`);
+      else if (this.authType === 'bearer') this.request.set('Authorization', `Bearer ${this.auth}`);
+      else if (this.authType === 'x-api-key') this.request.set('X-API-Key', this.auth);
+      else this.request.set('Authorization', this.auth);
+    }
+  }
+
+  private setCookies() {
+    if (this.cookies !== undefined) this.request.set('Cookie', this.cookies);
+  }
+
+  private setHeaders() {
+    this.request.set('Content-Type', 'application/json').timeout(this.timeout || 1500).retry(0);
+    if (this.responseType !== undefined) this.request.set('Response', this.responseType);
+    if (this.proxy !== undefined) this.request.auth['proxy'] = this.proxy;
+    if (this.redirect !== undefined) this.request.redirects(this.redirect);
+    if (this.accept !== undefined) this.request.set('Accept', this.accept);
+    if (this.connection !== undefined) this.request.set('Connection', this.connection);
+    if (this.cacheControl !== undefined) this.request.set('Cache Control', this.cacheControl);
   }
 
 
@@ -124,41 +150,29 @@ export default class apiHelper {
    */
   public async getRequest (endpoint: string, query?: object): Promise<apiTest> {
     const reqTime: string = moment().local().format('HH:mm:ss.SSS');
-    let request: superagentTypes.Request;
-    let response: superagentTypes.Response;
+    // let response: superagentTypes.Response;
 
     try {
-      request = superagent.get(`${this.baseUrl}/${endpoint}`);
+      this.request = superagent.get(`${this.baseUrl}/${endpoint}`);
       if (query) {
         for (const q in query) {
-          request.query(`${q}=${query[q]}`);
+          this.request.query(`${q}=${query[q]}`);
         }
       }
-      request.set('Content-Type', 'application/json').timeout(this.timeout || 1500).retry(0);
-      if (this.authType !== undefined) {
-        if (this.authType === 'basic') request.set('Authorization', `Basic ${this.auth}`);
-        else if (this.authType === 'bearer') request.set('Authorization', `Bearer ${this.auth}`);
-        else if (this.authType === 'x-api-key') request.set('X-API-Key', this.auth);
-        else request.set('Authorization', this.auth);
-      }
-      if (this.cookies !== undefined) request.set('Cookie', this.cookies);
-      if (this.responseType !== undefined) request.set('Response', this.responseType);
-      if (this.proxy !== undefined) request.auth['proxy'] = this.proxy;
-      if (this.redirect !== undefined) request.redirects(this.redirect);
-      if (this.accept !== undefined) request.set('Accept', this.accept);
-      if (this.connection !== undefined) request.set('Connection', this.connection);
-      if (this.cacheControl !== undefined) request.set('Cache Control', this.cacheControl);
+      this.setAuth();
+      this.setCookies();
+      this.setHeaders();
 
       // hack to show queries in mapReqResp output. Once request is submitted, this value is cleared out
-      const reqOrig = Object.fromEntries(Object.entries(request).map(([key, value]) => {
+      const reqOrig = Object.fromEntries(Object.entries(this.request).map(([key, value]) => {
         return [key, value];
       }));
 
-      response = await request.ok(res => res.status < 600);
+      this.response = await this.request.ok(res => res.status < 600);
 
       const respTime: string = moment().local().format('HH:mm:ss.SSS');
 
-      return this.mapReqResp(request, reqOrig, response, reqTime, respTime);
+      return this.mapReqResp(reqOrig, reqTime, respTime);
     }
     catch (error) {
       if (error instanceof Error) console.error(`ERROR: GET Request - ${error.message}`);
@@ -178,48 +192,35 @@ export default class apiHelper {
    */
   public async postRequest (endpoint: string, query?:object, body?: object|string|string[], file?:string[]): Promise<apiTest> {
     const reqTime: string = moment().local().format('HH:mm:ss.SSS');
-    let request: superagentTypes.Request;
-    let response: superagentTypes.Response;
 
     try {
-      request = superagent.post(`${this.baseUrl}/${endpoint}`);
+      this.request = superagent.post(`${this.baseUrl}/${endpoint}`);
       if (query !== undefined) {
         for (const q in query) {
-          request.query(`${q}=${query[q]}`);
+          this.request.query(`${q}=${query[q]}`);
         }
       }
       if (body !== undefined) {
-        request.send(body);
+        this.request.send(body);
       }
       if (file !== undefined) {
         const filePath = path.resolve(...file);
-        request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
+        this.request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
       }
-      if (this.authType !== undefined) {
-        if (this.authType === 'basic') request.set('Authorization', `Basic ${this.auth}`);
-        else if (this.authType === 'bearer') request.set('Authorization', `Bearer ${this.auth}`);
-        else if (this.authType === 'x-api-key') request.set('X-API-Key', this.auth);
-        else request.set('Authorization', this.auth);
-      }
-      if (this.cookies !== undefined) request.set('Cookie', this.cookies);
-      if (this.responseType !== undefined) request.set('Response', this.responseType);
-      if (this.proxy !== undefined) request.auth['proxy'] = this.proxy;
-      if (this.redirect !== undefined) request.redirects(this.redirect);
-      if (this.accept !== undefined) request.set('Accept', this.accept);
-      if (this.connection !== undefined) request.set('Connection', this.connection);
-      if (this.cacheControl !== undefined) request.set('Cache Control', this.cacheControl);
-
+      this.setAuth();
+      this.setCookies();
+      this.setHeaders();
       
       // hack to show queries in mapReqResp output. Once request is submitted, this value is cleared out
-      const reqOrig = Object.fromEntries(Object.entries(request).map(([key, value]) => {
+      const reqOrig = Object.fromEntries(Object.entries(this.request).map(([key, value]) => {
         return [key, value];
       }));
 
-      response = await request.ok(res => res.status < 600);
+      this.response = await this.request.ok(res => res.status < 600);
 
       const respTime: string = moment().local().format('HH:mm:ss.SSS');
 
-      return this.mapReqResp(request, reqOrig, response, reqTime, respTime);
+      return this.mapReqResp(reqOrig, reqTime, respTime);
     }
     catch (error) {
       if (error instanceof Error) console.error(`ERROR: POST Request - ${error.message}`);
@@ -238,52 +239,40 @@ export default class apiHelper {
    */
   public async putRequest (endpoint: string, query?:object, body?: object|string|string[], file?:string[]): Promise<apiTest> {
     const reqTime: string = moment().local().format('HH:mm:ss.SSS');
-    let request: superagentTypes.Request;
-    let response: superagentTypes.Response;
 
     try {
-      request = superagent.put(`${this.baseUrl}/${endpoint}`);
+      this.request = superagent.put(`${this.baseUrl}/${endpoint}`);
       if (query !== undefined) {
         for (const q in query) {
-          request.query(`${q}=${query[q]}`);
+          this.request.query(`${q}=${query[q]}`);
         }
       }
       if (body !== undefined) {
-        request.send(body);
+        this.request.send(body);
       }
       if (file !== undefined) {
         const filePath = path.resolve(...file);
 
-        request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
+        this.request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
       }
       if (file !== undefined) {
         const filePath = path.resolve(...file);
-        request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
+        this.request.set('Content-Type', 'multipart/form-data').attach('file', filePath);
       }
-      if (this.authType !== undefined) {
-        if (this.authType === 'basic') request.set('Authorization', `Basic ${this.auth}`);
-        else if (this.authType === 'bearer') request.set('Authorization', `Bearer ${this.auth}`);
-        else if (this.authType === 'x-api-key') request.set('X-API-Key', this.auth);
-        else request.set('Authorization', this.auth);
-      }
-      if (this.cookies !== undefined) request.set('Cookie', this.cookies);
-      if (this.responseType !== undefined) request.set('Response', this.responseType);
-      if (this.proxy !== undefined) request.auth['proxy'] = this.proxy;
-      if (this.redirect !== undefined) request.redirects(this.redirect);
-      if (this.accept !== undefined) request.set('Accept', this.accept);
-      if (this.connection !== undefined) request.set('Connection', this.connection);
-      if (this.cacheControl !== undefined) request.set('Cache Control', this.cacheControl);
+      this.setAuth();
+      this.setCookies();
+      this.setHeaders();
       
       // hack to show queries in mapReqResp output. Once request is submitted, this value is cleared out
-      const reqOrig = Object.fromEntries(Object.entries(request).map(([key, value]) => {
+      const reqOrig = Object.fromEntries(Object.entries(this.request).map(([key, value]) => {
         return [key, value];
       }));
 
-      response = await request.ok(res => res.status < 600);
+      this.response = await this.request.ok(res => res.status < 600);
 
       const respTime: string = moment().local().format('HH:mm:ss.SSS');
 
-      return this.mapReqResp(request, reqOrig, response, reqTime, respTime);
+      return this.mapReqResp(reqOrig, reqTime, respTime);
     }
     catch (error) {
       if (error instanceof Error) {
@@ -305,43 +294,31 @@ export default class apiHelper {
    */
   public async deleteRequest (endpoint: string, query?:object, body?: object|string|string[]): Promise<apiTest> {
     const reqTime: string = moment().local().format('HH:mm:ss.SSS');
-    let request: superagentTypes.Request;
-    let response: superagentTypes.Response;
 
     try {
-      request = superagent.put(`${this.baseUrl}/${endpoint}`);
+      this.request = superagent.put(`${this.baseUrl}/${endpoint}`);
       if (query !== undefined) {
         for (const q in query) {
-          request.query(`${q}=${query[q]}`);
+          this.request.query(`${q}=${query[q]}`);
         }
       }
       if (body !== undefined) {
-        request.send(body);
+        this.request.send(body);
       }
-      if (this.authType !== undefined) {
-        if (this.authType === 'basic') request.set('Authorization', `Basic ${this.auth}`);
-        else if (this.authType === 'bearer') request.set('Authorization', `Bearer ${this.auth}`);
-        else if (this.authType === 'x-api-key') request.set('X-API-Key', this.auth);
-        else request.set('Authorization', this.auth);
-      }
-      if (this.cookies !== undefined) request.set('Cookie', this.cookies);
-      if (this.responseType !== undefined) request.set('Response', this.responseType);
-      if (this.proxy !== undefined) request.auth['proxy'] = this.proxy;
-      if (this.redirect !== undefined) request.redirects(this.redirect);
-      if (this.accept !== undefined) request.set('Accept', this.accept);
-      if (this.connection !== undefined) request.set('Connection', this.connection);
-      if (this.cacheControl !== undefined) request.set('Cache Control', this.cacheControl);
+      this.setAuth();
+      this.setCookies();
+      this.setHeaders();
       
       // hack to show queries in mapReqResp output. Once request is submitted, this value is cleared out
-      const reqOrig = Object.fromEntries(Object.entries(request).map(([key, value]) => {
+      const reqOrig = Object.fromEntries(Object.entries(this.request).map(([key, value]) => {
         return [key, value];
       }));
 
-      response = await request.ok(res => res.status < 600);
+      this.response = await this.request.ok(res => res.status < 600);
 
       const respTime: string = moment().local().format('HH:mm:ss.SSS');
 
-      return this.mapReqResp(request, reqOrig, response, reqTime, respTime);
+      return this.mapReqResp(reqOrig, reqTime, respTime);
     }
     catch (error) {
       if (error instanceof Error) {
